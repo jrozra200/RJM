e9f78675241d3138855e010e367c88dbf05cf337
jrozra200 <rozran00@gmail.com>
2016-03-21 16:47:40 -0400

    Realized this is mostly a data cleaning exercise and focused on that

--- amazon_data.R
+++ amazon_data.R
@@ -1,16 +1,24 @@
-setwd("~/Google Drive/Grad School/Programming Practice/RJMetrics Interview/")
-
 library(plyr)
 
 amazonData <- read.csv("amazonReviews.csv")
 head(amazonData)
 summary(amazonData)
 
-amazonData$time <- as.POSIXct(amazonData$time, origin = "1970-01-01")
+## CHANGE THE TIME INTO HUMAN READABLE
+amazonData$time <- as.Date.POSIXct(amazonData$time, origin = "1970-01-01")
 
-########################################
-## SUSPICIOUS PATTERN: SERIAL REVIERS ##
-########################################
+## SPACE IN THE userId FIELD:
+amazonData[amazonData$userId == "AY12DBB0U420B", ]      ## GIVES NOTHING
+amazonData[amazonData$userId == " AY12DBB0U420B", ]     ## GIVES SOMETHING
+
+## HELPFULNESS SCORE IS A STRING
+new_level <- strsplit(as.character(amazonData$helpfulness), "/")
+new_level <- ldply(new_level, rbind)
+names(new_level) <- c("num_times_helpful", "num_people_voting")
+new_level$num_times_helpful <- as.numeric(as.character(new_level$num_times_helpful))
+new_level$num_people_voting <- as.numeric(as.character(new_level$num_people_voting))
+amazonData$helpfulness1 <- new_level$num_times_helpful / new_level$num_people_voting
+amazonData <- cbind(amazonData, new_level)
 
 count_by_userID <- ddply(amazonData, .(userId), summarize, 
                          profilename_count = length(unique(profileName)), 
@@ -19,29 +27,15 @@ count_by_userID <- ddply(amazonData, .(userId), summarize,
                          products_reviewed_count = length(unique(productId)),
                          avg_score = mean(score))
 
-# SOME IDEAS - PEOPLE REVIEWING TOO OFTEN
-## PEOPLE REVIEWING THE SAME PRODUCT OVER AND OVER
-## PEOPLE ARE ONLY GIVING POSITIVE REVIEWS
-## HOW LONG FOR THE GUY WHO DID 233 REVIEWS?
-## MARKING THINGS HELPFUL TOO OFTEN
+## FUNNY NAMES MAYBE TIPPING OFF SPOOF REVIEWS (LOOK FOR HIGH num_people_voting)
 
+## PEOPLE REVIEWING THE SAME PRODUCT MULTIPLE TIMES
+dim(count_by_userID[count_by_userID$total_reviews > count_by_userID$products_reviewed_count, ])
+head(count_by_userID[count_by_userID$total_reviews > count_by_userID$products_reviewed_count, ])
 
-############################################
-## SUSPICIOUS PATTERN: HELPFULNESS LEVELS ##
-############################################
+## MARKING THINGS HELPFUL OVER AND OVER
+dim(amazonData[!is.nan(amazonData$helpfulness1) & amazonData$helpfulness1 > 1, ])
+amazonData[!is.nan(amazonData$helpfulness1) & amazonData$helpfulness1 > 1, ]
 
-# Problem - 845 different helpfulness levels
-numlevels <- length(unique(amazonData$helpfulness))
-
-# Solution - change it to a percentage so it is normalized
-new_level <- strsplit(as.character(amazonData$helpfulness), "/")
-new_level <- ldply(new_level, rbind)
-names(new_level) <- c("num", "denom")
-new_level$num <- as.numeric(as.character(new_level$num))
-new_level$denom <- as.numeric(as.character(new_level$denom))
-amazonData$helpfulness1 <- new_level$num / new_level$denom
-amazonData <- cbind(amazonData, new_level)
-
-head(amazonData[!is.nan(amazonData$helpfulness1) & amazonData$helpfulness1 > 1, ])
-
-length(unique(amazonData$score))
+## HOW LONG FOR THE GUY WHO DID 233 REVIEWS? AnonReview - False positive
+## HOW LONG FOR THE GUY WHO DID 55 REVIEWS? GOOD AMOUNT OF TIME - False Positive
