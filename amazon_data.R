a72b41d874157324a2e137b16b72d760182cd1f2
jrozra200 <rozran00@gmail.com>
2016-03-21 19:15:25 -0400

    feedback on the amazon dataset

--- amazon_data.R
+++ amazon_data.R
@@ -10,6 +10,7 @@ amazonData$time <- as.Date.POSIXct(amazonData$time, origin = "1970-01-01")
 ## SPACE IN THE userId FIELD:
 amazonData[amazonData$userId == "AY12DBB0U420B", ]      ## GIVES NOTHING
 amazonData[amazonData$userId == " AY12DBB0U420B", ]     ## GIVES SOMETHING
+amazonData$userId <- gsub(pattern = " ", replacement = "", x = amazonData$userId)
 
 ## HELPFULNESS SCORE IS A STRING
 new_level <- strsplit(as.character(amazonData$helpfulness), "/")
@@ -20,6 +21,11 @@ new_level$num_people_voting <- as.numeric(as.character(new_level$num_people_voti
 amazonData$helpfulness1 <- new_level$num_times_helpful / new_level$num_people_voting
 amazonData <- cbind(amazonData, new_level)
 
+## HELPFULNESS SCORES THAT ARE NaN OR GREATER THAN 1
+amazonData$helpfulness1[is.nan(amazonData$helpfulness1)] <- 0
+amazonData$helpfulness1[amazonData$helpfulness1 > 1] <- 1
+
+## PEOPLE REVIEWING THE SAME PRODUCT MULTIPLE TIMES
 count_by_userID <- ddply(amazonData, .(userId), summarize, 
                          profilename_count = length(unique(profileName)), 
                          timerange = max(time) - min(time), 
@@ -27,15 +33,5 @@ count_by_userID <- ddply(amazonData, .(userId), summarize,
                          products_reviewed_count = length(unique(productId)),
                          avg_score = mean(score))
 
-## FUNNY NAMES MAYBE TIPPING OFF SPOOF REVIEWS (LOOK FOR HIGH num_people_voting)
-
-## PEOPLE REVIEWING THE SAME PRODUCT MULTIPLE TIMES
 dim(count_by_userID[count_by_userID$total_reviews > count_by_userID$products_reviewed_count, ])
 head(count_by_userID[count_by_userID$total_reviews > count_by_userID$products_reviewed_count, ])
-
-## MARKING THINGS HELPFUL OVER AND OVER
-dim(amazonData[!is.nan(amazonData$helpfulness1) & amazonData$helpfulness1 > 1, ])
-amazonData[!is.nan(amazonData$helpfulness1) & amazonData$helpfulness1 > 1, ]
-
-## HOW LONG FOR THE GUY WHO DID 233 REVIEWS? AnonReview - False positive
-## HOW LONG FOR THE GUY WHO DID 55 REVIEWS? GOOD AMOUNT OF TIME - False Positive
